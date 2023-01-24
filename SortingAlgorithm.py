078bd1776fe323f264af31561c1ca37b7e3a5a58
Wu Sai <wusai.ustc@outlook.com>
2023-01-24 20:41:48 +0800

    lab2-排序算法训练完成

---
+++ SortingAlgorithm.py
@@ -0,0 +1,39 @@
+def quick_sort(array, p, r):
+    """
+    :param array: 待排序数组
+    :param p: 待排序数组的开始下标
+    :param r: 待排序数组的结束下标
+    :return: 无
+    """
+    if p < r:
+        q = partition(array, p, r)
+        quick_sort(array, p, q - 1)
+        quick_sort(array, q + 1, r)
+
+
+def counting_sort(array_a, array_b, k):
+    """
+    :param array_a: 待排序数组
+    :param array_b: 排序好的数组
+    :param k: 待排序数组元素的最大值 + 1
+    :return: 无
+    """
+    array_c = [0 for i in range(k)]
+    for i in array_a:
+        array_c[i] += 1
+    for i in range(1, len(array_c)):
+        array_c[i] += array_c[i - 1]
+    for i in array_a[::-1]:
+        array_b[array_c[i]-1] = i
+        array_c[i] -= 1
+
+
+def partition(array, p, r):
+    x = array[r]
+    j = p - 1
+    for i in range(p, r):
+        if array[i] <= x:
+            j += 1
+            array[i], array[j] = array[j], array[i]
+    array[j + 1], array[r] = array[r], array[j + 1]
+    return j + 1
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import SortingAlgorithm
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # 测试快速排序
+    print("==================== 测试快速排序 ====================")
+    array = [random.randint(0, 100) for i in range(10)]
+    print("待排序数组: ", array)
+    SortingAlgorithm.quick_sort(array, 0, 8)
+    print("排序后数组: ", array)
+    # 测试计数排序
+    print("==================== 测试计数排序 ====================")
+    array_a = [random.randint(0, 100) for i in range(10)]
+    print("待排序数组: ", array_a)
+    array_b = [0 for i in range(10)]
+    SortingAlgorithm.counting_sort(array_a, array_b, max(array_a) + 1)
+    print("排序后数组: ", array_b)
