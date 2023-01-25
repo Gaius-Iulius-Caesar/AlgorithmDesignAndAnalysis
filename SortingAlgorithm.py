cb4f39ec3c05f776c16153b016a6fe297c2dbad6
Wu Sai <wusai.ustc@outlook.com>
2023-01-25 16:03:08 +0800

    修改一些bug

--- SortingAlgorithm.py
+++ SortingAlgorithm.py
@@ -15,17 +15,18 @@ def counting_sort(array_a, array_b, k):
     """
     :param array_a: 待排序数组
     :param array_b: 排序好的数组
-    :param k: 待排序数组元素的最大值 + 1
+    :param k: 待排序数组元素的范围值 + 1
     :return: 无
     """
+    mina = min(array_a)
     array_c = [0 for i in range(k)]
     for i in array_a:
-        array_c[i] += 1
+        array_c[i-mina] += 1
     for i in range(1, len(array_c)):
         array_c[i] += array_c[i - 1]
     for i in array_a[::-1]:
-        array_b[array_c[i]-1] = i
-        array_c[i] -= 1
+        array_b[array_c[i-mina]-1] = i
+        array_c[i-mina] -= 1
 
 
 def partition(array, p, r):
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import SortingAlgorithm
-import random
 
 if __name__ == '__main__':
     # 测试快速排序
     print("=================== 测试快速排序 =====================")
     array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
     print("待排序数组: ", array)
-    SortingAlgorithm.quick_sort(array, 0, 8)
+    SortingAlgorithm.quick_sort(array, 0, len(array) - 1)
     print("排序后数组: ", array)
     # 测试计数排序
     print("=================== 测试计数排序 =====================")
     array_a = [95, 94, 91, 98, 99, 90, 99, 93, 91, 92]
     print("待排序数组: ", array_a)
-    array_b = [0 for i in range(10)]
-    SortingAlgorithm.counting_sort(array_a, array_b, max(array_a) + 1)
+    array_b = [0 for i in range(len(array_a))]
+    SortingAlgorithm.counting_sort(array_a, array_b, max(array_a) - min(array_a) + 1)
     print("排序后数组: ", array_b)
