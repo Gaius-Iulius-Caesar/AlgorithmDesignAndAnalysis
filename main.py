b789588f11b95ff902c779e663099396a62f3c38
Wu Sai <wusai.ustc@outlook.com>
2023-01-25 15:18:38 +0800

    修改一些bug

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import random
 
 if __name__ == '__main__':
     # 测试快速排序
-    print("==================== 测试快速排序 ====================")
-    array = [random.randint(0, 100) for i in range(10)]
+    print("=================== 测试快速排序 =====================")
+    array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
     print("待排序数组: ", array)
     SortingAlgorithm.quick_sort(array, 0, 8)
     print("排序后数组: ", array)
     # 测试计数排序
-    print("==================== 测试计数排序 ====================")
-    array_a = [random.randint(0, 100) for i in range(10)]
+    print("=================== 测试计数排序 =====================")
+    array_a = [95, 94, 91, 98, 99, 90, 99, 93, 91, 92]
     print("待排序数组: ", array_a)
     array_b = [0 for i in range(10)]
     SortingAlgorithm.counting_sort(array_a, array_b, max(array_a) + 1)
